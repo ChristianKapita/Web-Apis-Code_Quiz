d7aadac31f1b916cb392ff230ed0cf0e7e811d6f
Chris Zip <kapitachristian2003@gmail.com>
2020-12-05 22:23:20 -0500

    JsForScore.js created -logic to display score on scores.html

--- Assets/JavaScript/JsForScore.js
+++ Assets/JavaScript/JsForScore.js
@@ -1,4 +1,4 @@
-unction printHighscores() {
+function printHighscores() {
     // either get scores from localstorage or set to empty array
     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
   
